433a5399444bb7b2a61731e4e21efd31b475feb1
Some One <someone@example.com>
2024-12-06 22:06:14 +0100

    initial set up

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿let get_content file =
+    System.IO.File.ReadLines(file)
+
+let get_field (line:string) =
+    let fields = line.Split '\t'
+    if Array.length fields > 2 then
+        fields.[1]
+    else
+        ""
+
+[<EntryPoint>]
+let main(args) =    
+    printfn "args: %A" args
+    Array.last args
+    |> get_content
+    |> Seq.map get_field
+    |> Seq.iter (printfn "%s")
+    0
---
+++ README.md
@@ -0,0 +1,6 @@
+# cuttool
+Program your own `cut` https://codingchallenges.fyi/challenges/challenge-cut/
+
+## using jj
+- after adding file to .gitignore remove it from tracking
+  `jj file untrack <file>`
