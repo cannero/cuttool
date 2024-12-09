edd2b7375e4c30f4a946ce61fa84d512649359c5
Some One <someone@example.com>
2024-12-09 21:16:02 +0100

    hyphen for range

--- CommandLine.fs
+++ CommandLine.fs
@@ -1,11 +1,30 @@
 module Commandline
     open System
 
+    // Which fields should be printed, 1-based
+    type FieldType =
+        | Of of field : int
+        | From of field : int
+        | To of field : int
+        | FromTo of fieldFrom : int * fieldTo : int
+
     type CommandLineOptions = {
-        fields: int list
+        fields: FieldType list
         delimiter: string
         filename: string
     }
+
+    let getField (field: string) =
+        let field = field.Trim()
+        if field.StartsWith('-') then
+            To(field = max 1 (field.Substring(1) |> int))
+        elif field.EndsWith('-') then
+            From(field = max 1 ((field.Substring(0, (String.length field - 1))) |> int))
+        elif field.Contains('-') then
+            let parts = field.Split('-')
+            FromTo(fieldFrom = max 1 (parts.[0] |> int), fieldTo = max 1 (parts.[1] |> int))
+        else
+            Of(field = max 1 (field |> int))
 
     let getFields (fieldString: string) =
         let fields =
@@ -15,7 +34,7 @@ module Commandline
                 fieldString.Split(' ', StringSplitOptions.RemoveEmptyEntries)
         fields
         |> Array.toList
-        |> List.map int
+        |> List.map getField
 
     let rec parseCmdLineRec args options =
         match args with
@@ -43,7 +62,7 @@ module Commandline
 
     let parseCmdLine args =
         let defaultOptions = {
-            fields = [1]
+            fields = [Of(1)]
             delimiter = "\t"
             filename = ""
         }
--- Program.fs
+++ Program.fs
@@ -8,22 +8,35 @@ let getContentStdin () =
     |> Seq.initInfinite
     |> Seq.takeWhile ((<>) null)
 
-let getField fields field =
-    if Array.length fields > field-1 then
-        fields.[field-1]
-    else
-        ""
-
+let getField (fields: string array) fieldType =
+    let len = Array.length fields
+    match fieldType with
+        | Of(field) ->
+            if len > field-1 then
+                seq { fields.[field-1] }
+            else
+                seq {""}
+        | To(field) ->
+            seq { 0 .. (min field (len - 1)) }
+            |> Seq.map (fun i -> fields.[i])
+        | From(field) ->
+            seq { field - 1 .. len - 1 }
+            |> Seq.map (fun i -> fields.[i])
+        | FromTo(fieldFrom, fieldTo) ->
+            seq { fieldFrom - 1 .. (min (len - 1) fieldTo) }
+            |> Seq.map (fun i -> fields.[i])
 
 let getFields options (line:string) =
     let fields = line.Split options.delimiter
-    options.fields |> List.map (getField fields)
-
+    options.fields
+    |> List.map (getField fields)
+    |> Seq.concat
 
 let printFields options getFunc =
     getFunc
     |> Seq.map (getFields options)
-    |> Seq.iter (printfn "%A")
+    |> Seq.map (String.concat options.delimiter)
+    |> Seq.iter (printfn "%s")
 
 [<EntryPoint>]
 let main(args) =    
